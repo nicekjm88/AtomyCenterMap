2558f8d0f7b20e2776d112e566c5e0ad7ce9ce37
jmkim88 <nicekjm88@gmail.com>
2021-05-20 11:16:08 +0900

    - 스타일수정 - 검색내용없음 추가

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var spinner = document.getElementById("spinner");
+const spinner = document.getElementById("spinner");
 
-var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+const mapContainer = document.getElementById("map"), // 지도를 표시할 div
   mapOption = {
     center: new kakao.maps.LatLng(36.503603, 127.250751), // 지도의 중심좌표
     level: 12, // 지도의 확대 레벨
@@ -8,25 +8,25 @@ var mapContainer = document.getElementById("map"), // 지도를 표시할 div
   };
 
 // 지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption);
+const map = new kakao.maps.Map(mapContainer, mapOption);
 
 // 마커 클러스터러를 생성합니다
-var clusterer = new kakao.maps.MarkerClusterer({
+const clusterer = new kakao.maps.MarkerClusterer({
   map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
   averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
   minLevel: 8, // 클러스터 할 최소 지도 레벨
 });
 
 // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-var zoomControl = new kakao.maps.ZoomControl();
+const zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 // 검색기능
-var inputTxt = document.querySelector("#keyword");
+const inputTxt = document.querySelector("#keyword");
 
 function panTo(lat, lng, centerName, address1, address2) {
   // 이동할 위도 경도 위치를 생성합니다
-  var moveLatLon = new kakao.maps.LatLng(lat, lng);
+  const moveLatLon = new kakao.maps.LatLng(lat, lng);
 
   map.setLevel(4);
 
@@ -34,17 +34,17 @@ function panTo(lat, lng, centerName, address1, address2) {
   // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
   map.panTo(moveLatLon);
 
-  var content = `<div class="overlay_info">
+  const content = `<div class="overlay_info">
                   <p class="title">센터명 : <strong>${centerName}</strong></p>
                   <div class="desc">
                     <span class="address">${address1}&nbsp;${address2}</span>
                   </div>
                 </div>`;
 
-  var position = moveLatLon;
+  const position = moveLatLon;
 
   // 커스텀 오버레이를 생성합니다
-  var mapCustomOverlay = new kakao.maps.CustomOverlay({
+  const mapCustomOverlay = new kakao.maps.CustomOverlay({
     position: position,
     content: content,
     xAnchor: 0.5, // 커스텀 오버레이의 x축 위치입니다. 1에 가까울수록 왼쪽에 위치합니다. 기본값은 0.5 입니다
@@ -56,7 +56,7 @@ function panTo(lat, lng, centerName, address1, address2) {
 
   kakao.maps.event.addListener(map, "zoom_changed", function () {
     // 지도의 현재 레벨을 얻어옵니다
-    var level = map.getLevel();
+    const level = map.getLevel();
 
     if (level > 4) {
       mapCustomOverlay.setMap(null);
@@ -68,20 +68,22 @@ function panTo(lat, lng, centerName, address1, address2) {
 
 async function loadData() {
   spinner.removeAttribute("hidden");
-  await fetch("./centerData.json")
+  await fetch(
+    "https://atomy-kr-education-center-default-rtdb.asia-southeast1.firebasedatabase.app/.json"
+  )
     .then((response) => response.json())
     .then((data) => {
       spinner.setAttribute("hidden", "");
-      var markers = $(data).map(function (i, data) {
-        var coords = new kakao.maps.LatLng(data.lat, data.lng);
+      const markers = $(data).map(function (i, data) {
+        const coords = new kakao.maps.LatLng(data.lat, data.lng);
 
-        var marker = new kakao.maps.Marker({
+        const marker = new kakao.maps.Marker({
           map: map,
           position: coords,
         });
 
         // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
+        const infowindow = new kakao.maps.InfoWindow({
           content: `<div class="wrap-infowindow">
                       <strong>${data.centerName}</strong>
                       <ul>
@@ -126,31 +128,47 @@ async function loadData() {
       }
 
       inputTxt.addEventListener("change", function (e, i) {
-        var keyword = e.target.value;
+        const keyword = e.target.value;
 
-        var result = data.filter((e) => {
-          var isContain =
+        const result = data.filter((e) => {
+          const isContain =
             e.centerName.includes(keyword) ||
             e.address1.includes(keyword) ||
             e.address2.includes(keyword);
           return isContain;
         });
 
-        var listGroup = document.querySelector("#placesList");
-        var itemStr = "";
+        const listGroup = document.querySelector("#placesList");
 
-        for (let i = 0; i < result.length; i++) {
-          itemStr += `<li>
-                      <a href="#" onclick="panTo(${result[i].lat},${result[i].lng},'${result[i].centerName}','${result[i].address1}','${result[i].address2}')">
-                        <strong>${result[i].centerName}</strong>
-                        <address>${result[i].address1}&nbsp;${result[i].address2}</address>
-                      </a>
-                    </li>`;
+        if (result.length === 0 || keyword === "") {
+          if (!$(".nodata").is(":visible")) {
+            listGroup.innerHTML = "";
+            listGroup.insertAdjacentHTML(
+              "beforebegin",
+              "<div class='nodata'>데이터가 존재하지 않습니다.</div>"
+            );
+          }
+        } else {
+          if ($(".nodata").is(":visible")) {
+            $(".nodata").remove();
+          }
+
+          let itemStr = "";
+
+          for (let i = 0; i < result.length; i++) {
+            itemStr += `<li>
+                        <a href="#" onclick="panTo(${result[i].lat},${result[i].lng},'${result[i].centerName}','${result[i].address1}','${result[i].address2}')">
+                          <strong>${result[i].centerName}</strong>
+                          <address>${result[i].address1}&nbsp;${result[i].address2}</address>
+                        </a>
+                      </li>`;
+          }
+
+          listGroup.innerHTML = itemStr;
         }
-
-        listGroup.innerHTML = itemStr;
       });
-    });
+    })
+    .catch((error) => console.log(error));
 }
 
 loadData();
